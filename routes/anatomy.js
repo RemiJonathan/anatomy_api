b420e737a659628ac5c315e2569c47017faa7082
Remi Jonathan Choquette <contact@remijonathan.com>
2022-12-19 11:36:51 -0500

    Modified the endpoint to make it point to the structures.json file

--- routes/anatomy.js
+++ routes/anatomy.js
@@ -1,31 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
 /*GET anatomy JSON Object*/
 router.get('/', function (req, res, next) {
-    //TODO: Use fs to read the file and return the JSON object
-    res.json({
-        anatomy: {
-            structures: [
-                {name: 'General anatomy'},
-                {name: 'Bones'},
-                {name: 'Joints'},
-                {name: 'Muscles'},
-                {name: 'Alimentary system'},
-                {name: 'Respiratory system'},
-                {name: 'Thoracic cavity'},
-                {name: 'Urinary system'},
-                {name: 'Genital systems'},
-                {name: 'Abdominopelvic cavity'},
-                {name: 'Endocrine glands'},
-                {name: 'Cardiovascular system'},
-                {name: 'Lymphoid system'},
-                {name: 'Nervous system'},
-                {name: 'Sense organs'},
-                {name: 'The integument'}
-
-            ]
+    //Use fs to read the file and return the JSON object
+    fs.readFile('./data/structures.json', 'utf8', function (err, data) {
+        if (err) {
+            console.log(err);
         }
+        res.send(JSON.parse(data));
     });
 });
 
